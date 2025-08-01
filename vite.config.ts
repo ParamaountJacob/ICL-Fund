9b7c97e1dc512eb974905092bb32b42203243fe2
ParamaountJacob <JacobGriswold0@gmail.com>
2025-08-01 16:26:31 -0500

    1

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    host: '0.0.0.0'
+    port: 5777,
+    host: '0.0.0.0',
+    strictPort: false
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
